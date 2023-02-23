1725d85fc056f01d8dccb0a3c75852035e630b27
CGUEVARA03 <usis037621gb.edu.sv>
2023-02-23 07:57:54 -0600

    Parcial

--- app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
+++ app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
-
+        TabHost tbthost;
 
 
 
@@ -18,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tbthost= (TabHost) findViewById(R.id.tabhost);
+        tbthost.setup();
+
+       TabHost.TabSpec tab1 = tbthost.newTabSpec("tab1");
+       tab1.setIndicator("Conversor");
+       tab1.setContent(R.id.tab1);
+
+        TabHost.TabSpec tab2 = tbthost.newTabSpec("tab2");
+        tab2.setIndicator("Lectura");
+        tab2.setContent(R.id.tab2);
+
+        tbthost.addTab(tab1);
+        tbthost.addTab(tab2);
 
 
 
