109035dc1a5dae6b25c8b500c6b5be6c60b45dc1
CGUEVARA03 <usis037621gb.edu.sv>
2023-02-23 08:27:47 -0600

    Parcial

--- app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
+++ app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
@@ -6,11 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
         TabHost tbthost;
+
+        Button btnconvertir;
+
+        TextView temp;
+
+        Spinner spn;
+
+        conversor miConversor = new conversor();
 
 
 
@@ -35,6 +45,43 @@ public class MainActivity extends AppCompatActivity {
         tbthost.addTab(tab2);
 
 
+        btnconvertir = (Button) findViewById(R.id.btnconvert);
+        btnconvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                temp = (TextView) findViewById(R.id.txtcantidad);
+                double cantidad = Double.parseDouble(temp.getText().toString());
+
+                spn =findViewById(R.id.spn1);
+                int de = spn.getSelectedItemPosition();
+
+                spn =findViewById(R.id.spn2);
+                int a = spn.getSelectedItemPosition();
+
+                temp = findViewById(R.id.txtresultado);
+                temp.setText("Resultado"+ miConversor.conversor(0,de, a, cantidad));
+
+            }
+        });
+
+
+
+
+
 
     }
+}
+
+class conversor{
+    double [][] superficie = {
+        {1, 0.1111, 0.111111, 0.09290304, 0.0001477465648855, 0.00001317, 9.2903e-6}
+
+    };
+    public double conversor(int opcion, int de, int a, double cantidad){
+        return superficie[opcion][a]/ superficie[opcion][de]* cantidad;
+    }
+
+
+
 }
