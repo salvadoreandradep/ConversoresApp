79d34a8624addc71f438d6fcc0da93900f345cfd
salvadoreandradep <salvadoreandradep>
2023-04-13 17:08:57 -0600

    Nueva Rama Couch

--- app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
+++ app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
@@ -2,23 +2,12 @@ package com.gruposeven.conversoresapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-    Button monedas;
-    Button volumen;
-
-    Button longitud;
-
-    Button datostbtn;
-
-    Button bttime;
 
 
 
@@ -26,72 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        bttime=(Button)findViewById(R.id.btnTiempo);
-        bttime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent t = new Intent(MainActivity.this, Tiempo.class);
-                startActivity(t);
-            }
-        });
-
-
-        datostbtn=(Button)findViewById(R.id.btnDatos) ;
-
-        datostbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent d = new Intent(MainActivity.this, Datos.class);
-                startActivity(d);
-            }
-        });
-
-
-
-        longitud=(Button)findViewById(R.id.btnlongitud);
-
-        longitud.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent l = new Intent (MainActivity.this, Longitud.class);
-                startActivity(l);
-
-            }
-        });
-
-
-
-
-        monedas = (Button)findViewById(R.id.btnmonedas);
-
-        monedas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent m = new Intent (MainActivity.this, Monedas.class);
-                startActivity(m);
-
-
-
-            }
-        });
-
-
-
-        volumen = (Button)findViewById(R.id.btnvolumen);
-
-        volumen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent v = new Intent (MainActivity.this, Volumen.class);
-                startActivity(v);
-
-            }
-        });
-
 
 
 
