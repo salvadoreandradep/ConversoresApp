2eb651324a899496a5e5e23e5a068c04f7109cb7
salvadoreandradep <salvadoreandradep>
2023-04-18 17:46:33 -0600

    V2

--- app/src/main/java/com/gruposeven/conversoresapp/agregar_catalogo.java
+++ app/src/main/java/com/gruposeven/conversoresapp/agregar_catalogo.java
@@ -3,12 +3,40 @@ package com.gruposeven.conversoresapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 public class agregar_catalogo extends AppCompatActivity {
+    String accion = "nuevo";
+    String id = "";
+    String rev = "";
+    JSONObject datosJSON;
+    String resp;
+
+    TextView temp;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_catalogo);
+
+        Bundle parametros = getIntent().getExtras();
+        accion = parametros.getString("accion");
+
+        if (accion.equals("modificar")){
+            try {
+                datosJSON = new JSONObject(parametros.getString("valores"));
+
+                TextView temp = (TextView) findViewById(R.id.)
+            }catch (Exception e) {
+
+
+            }
+
+
+        }
     }
 }
