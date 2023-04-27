ebb4e2a04c1780f573c079d4d781fc579f5f824e
salvadoreandradep <salvadorplay7089@gmail.com>
2023-04-26 22:40:22 -0600

    Paso 1

--- app/src/main/java/com/gruposeven/conversoresapp/AgregarProducto.java
+++ app/src/main/java/com/gruposeven/conversoresapp/AgregarProducto.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,11 +21,28 @@ import androidx.core.content.FileProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AgregarProducto extends AppCompatActivity {
+
+
+
+    String id = "";
+    String rev = "";
+    JSONObject datosJSON;
+    String resp;
     FloatingActionButton btnAtras;
     ImageView imgFotoAmigo;
     Intent tomarFotoIntent;
@@ -35,7 +55,94 @@ public class AgregarProducto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_repuesto);
+        btn = findViewById(R.id.btnSaveRep);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+
+                TextView temp = (TextView) findViewById(R.id.txtNombre);
+                String nombre2 = temp.getText().toString();
+
+                temp = (TextView) findViewById(R.id.txtCategoria);
+                String direccion2 = temp.getText().toString();
+
+                temp = (TextView) findViewById(R.id.txtMarca);
+                String telefono2 = temp.getText().toString();
+
+                temp = (TextView) findViewById(R.id.txtPrecio);
+                String dui2 = temp.getText().toString();
+
+
+
+                JSONObject miData = new JSONObject();
+                try {
+                    if (accion.equals("modificar")){
+                        miData.put("_id", id);
+                        miData.put("_rev", rev);
+                    }
+
+                    miData.put("nombre", nombre2);
+                    miData.put("direccion", direccion2);
+                    miData.put("telefono", telefono2);
+                    miData.put("dui", dui2);
+
+                    AgregarProducto.enviardatos objenviar = new enviardatos();
+                    objenviar.execute(miData.toString());
+
+
+
+                }catch (Exception ex) {
+                    Toast.makeText(AgregarProducto.this, "Error", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        Bundle parametros = getIntent().getExtras();
+        accion = parametros.getString("accion");
+
+        if (accion.equals("modificar")){
+            try {
+                datosJSON = new JSONObject(parametros.getString("valores"));
+
+
+
+                TextView temp = (TextView) findViewById(R.id.txtNombre);
+                temp.setText(datosJSON.getString("nombre"));
+
+                temp = (TextView) findViewById(R.id.txtCategoria);
+                temp.setText(datosJSON.getString("marca"));
+
+                temp = (TextView) findViewById(R.id.txtMarca);
+                temp.setText(datosJSON.getString("presentacion"));
+
+                temp = (TextView) findViewById(R.id.txtPrecio);
+                temp.setText(datosJSON.getString("precio"));
+
+                id = datosJSON.getString("_id");
+                rev = datosJSON.getString("_rev");
+
+            }catch (Exception e) {
+                Toast.makeText(this, "Error al recuperar datos: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+
+
+
+
+            Button btnregresar = (Button) findViewById(R.id.btnSaveRep);
+            btnregresar.setOnClickListener(view1 -> {
+                Intent r = new Intent(AgregarProducto.this, MainActivity.class);
+                startActivity(r);
+
+
+            });
+
+        }
         miBD = new DB(getApplicationContext(),"",null,1);
         btnAtras = findViewById(R.id.btnAtras);
         btnAtras.setOnClickListener(v->{
@@ -147,5 +254,89 @@ public class AgregarProducto extends AppCompatActivity {
     }
     private void mostrarMsgToast(String msg){
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+
+    private class enviardatos extends AsyncTask<String, String, String> {
+        HttpURLConnection urlConnection;
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder result = new StringBuilder();
+
+            String JsonResponse = null;
+            String JsonDATA = params[0];
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL("http://192.168.100.3:5984/db_catalogo/%22");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+
+
+                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(),"UTF-8"));
+                writer.write(JsonDATA);
+                writer.close();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                if (inputStream == null){
+
+                    return  null;
+
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                resp = reader.toString();
+                String inputLine;
+                StringBuffer buffer = new StringBuffer();
+                while ((inputLine = reader.readLine()) != null );
+                buffer.append(inputLine + "\n");
+
+                if (buffer.length() == 0){
+
+                    return  "";
+                }
+
+                JsonResponse = buffer.toString();
+
+                Log.i(id, JsonResponse);
+
+                return JsonResponse;
+
+            }catch (Exception ex){
+                ex.printStackTrace();
+
+
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                if (jsonObject.getBoolean("ok")){
+
+                    Toast.makeText(AgregarProducto.this, "Registro enviado", Toast.LENGTH_SHORT).show();
+                    Intent regresar = new Intent(AgregarProducto.this, MainActivity.class);
+                    startActivity(regresar);
+
+
+                }else {
+                    Toast.makeText(AgregarProducto.this, "Error al intentar almacenar el registro...", Toast.LENGTH_SHORT).show();
+                }
+
+            }catch (Exception e) {
+                Toast.makeText(AgregarProducto.this, "Error al enviar a la red" + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
 }
--- app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
+++ app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
@@ -1,16 +1,20 @@
 package com.gruposeven.conversoresapp;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,9 +24,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Bundle parametros = new Bundle();
+
+
+    JSONArray datosJSON;
+    JSONObject jsonObject;
+
+    int posicion=0;
+
+
+
+    InputStreamReader isReader;
     FloatingActionButton btnFloat;
     DB miBD;
     ListView ltsProducto;
@@ -34,6 +59,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        obtenerDatos myAsync = new obtenerDatos();
+        myAsync.execute();
+
+        FloatingActionButton btn = (FloatingActionButton) findViewById(R.id.btnAgregarAmigos);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parametros.putString("accion", "nuevo");
+                nuevo_catalogo();
+            }
+        });
+
+
+
 
         btnFloat = findViewById(R.id.btnAgregarAmigos);
         btnFloat.setOnClickListener(v->{
@@ -47,7 +88,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_repuestos, menu);
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        try {
 
+            datosJSON.getJSONObject(info.position);
+            menu.setHeaderTitle(datosJSON.getJSONObject(info.position).getJSONObject("value").getString("nombre").toString());
+            posicion = info.position;
+
+        }catch (Exception e){
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
         AdapterView.AdapterContextMenuInfo adapterContextMenuInfo = (AdapterView.AdapterContextMenuInfo)menuInfo;
         datosProductosCursor.moveToPosition(adapterContextMenuInfo.position);
         menu.setHeaderTitle(datosProductosCursor.getString(1));
@@ -192,7 +242,161 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
     }
 
+    public void  nuevo_catalogo(){
+        Intent agregar_catalogo = new Intent(MainActivity.this, AgregarProducto.class);
+        agregar_catalogo.putExtras(parametros);
+        startActivity(agregar_catalogo);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private class obtenerDatos extends AsyncTask<Void, Void, String> {
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            StringBuilder result = new StringBuilder();
+
+            try {
+                URL url = new URL("http://192.168.100.3:5984/db_catalogo/_design/catalogo/_view/mi_catalogo");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+
+                InputStream in = new BufferedInputStream(urlConnection.getErrorStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                while ((line = reader.readLine()) != null)
+                {
+                    result.append(line);
+                }
+
+            } catch (Exception ex) {
+                Log.e("Mi Error", "Error", ex);
+                ex.printStackTrace();
+
+            } finally {
+                urlConnection.disconnect();
+            }
+
+
+            return result.toString();
+        }
+
+
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                jsonObject = new JSONObject(s);
+                datosJSON = jsonObject.getJSONArray("rows");
+
+                ListView lstCatalogo = (ListView) findViewById(R.id.ltsamigos);
+
+                final ArrayList<String> alAgenda = new ArrayList<String>();
+                final ArrayAdapter<String> aaAgenda = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, alAgenda);
+
+                lstCatalogo.setAdapter(aaAgenda);
+
+                for (int i=0; i<datosJSON.length(); i++){
+                    alAgenda.add(datosJSON.getJSONObject(i).getJSONObject("value").getString("nombre").toString());
+
+                }
+                aaAgenda.notifyDataSetChanged();;
+                registerForContextMenu(lstCatalogo);
+
+
+            } catch (Exception ex) {
+                Toast.makeText(MainActivity.this, "Error" + ex.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
+
+    private class eliminarDatos extends AsyncTask<String, String, String>{
+        HttpURLConnection urlConnection;
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder result = new StringBuilder();
+
+
+            String JsonResponse = null;
+            String JsonDATA = params[0];
+            BufferedReader reader = null;
+
+            try{
+                String uri = "http://192.168.100.3:5984/db_catalogo/"+
+                        datosJSON.getJSONObject(posicion).getJSONObject("value").getString("_id")+ "?rev="+ datosJSON.getJSONObject(posicion).getJSONObject("value").getString("_rev");
+                URL url = new URL(uri);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("DELETE");
+
+                InputStream in = new BufferedInputStream(urlConnection.getErrorStream());
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line ;
+                while ((line = reader.readLine()) !=null){
+                    result.append(line);
+
+                }
+
+
+
+            } catch (Exception ex){
+
+                Log.e("Mi Error", "Error", ex);
+                ex.printStackTrace();
+
+            }finally {
+                urlConnection.disconnect();
+            }
+            return result.toString();
+        }
+
+
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject1 = new JSONObject(s);
+
+                if (jsonObject1.getBoolean("ok")){
+                    Toast.makeText(MainActivity.this, "Registro Eliminado", Toast.LENGTH_SHORT).show();
+                    Intent regresar = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(regresar);
+                }else {
+                    Toast.makeText(MainActivity.this, "Error al Eliminar Registro...", Toast.LENGTH_SHORT).show();
+                }
+
+            }catch (Exception ex){
+                Toast.makeText(MainActivity.this, "Error al enviar a la red", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+
 }
+
+
 
 class Productos {
     String idProducto;
