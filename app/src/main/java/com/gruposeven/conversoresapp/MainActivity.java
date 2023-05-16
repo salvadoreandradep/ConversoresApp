81f9c35530ef1f781a311e26fa2b3f629aa8d194
salvadoreandradep <salvadorplay7089@gmail.com>
2023-05-15 21:05:21 -0600

    Diseño

--- app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
+++ app/src/main/java/com/gruposeven/conversoresapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     TextView btntext, btntex2;
