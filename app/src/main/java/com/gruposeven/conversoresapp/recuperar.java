08f817790bce577d4af548d4ae3b69c18edab3ed
salvadoreandradep <salvadorplay7089@gmail.com>
2023-05-15 20:32:25 -0600

    Diseño

--- app/src/main/java/com/gruposeven/conversoresapp/recuperar.java
+++ app/src/main/java/com/gruposeven/conversoresapp/recuperar.java
@@ -2,13 +2,28 @@ package com.gruposeven.conversoresapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class recuperar extends AppCompatActivity {
+
+    TextView ya;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar);
+
+        ya=findViewById(R.id.txtrecupe);
+        ya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regresar = new Intent(recuperar.this, MainActivity.class);
+                startActivity(regresar);
+            }
+        });
+
     }
 }
