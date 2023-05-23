be7079802a61b8d2ff50ddcf909ccec5a5526bf4
salvadoreandradep <salvadorplay7089@gmail.com>
2023-05-22 20:56:16 -0600

    Usuario

--- app/src/main/java/com/gruposeven/conversoresapp/SignUpActivity.java
+++ app/src/main/java/com/gruposeven/conversoresapp/SignUpActivity.java
@@ -17,78 +17,69 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText nombreEditText, correoEditText, contraseñaEditText;
+    private EditText edtNombre;
+    private EditText edtApodo;
+    private EditText edtCorreo;
+    private EditText edtContraseña;
+    private Button btnRegistrar;
 
-    TextView regresarlogin;
-    Button registrarButton;
-    ;
-    FirebaseAuth firebaseAuth;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        nombreEditText = findViewById(R.id.nombreEditText);
-        correoEditText = findViewById(R.id.correoEditText);
-        contraseñaEditText = findViewById(R.id.contraseñaEditText);
-        registrarButton = findViewById(R.id.registrarButton);
-        regresarlogin=findViewById(R.id.textViewLogin);
 
-        regresarlogin.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        edtNombre = findViewById(R.id.edtNombre);
+        edtApodo = findViewById(R.id.edtApodo);
+        edtCorreo = findViewById(R.id.edtCorreo);
+        edtContraseña = findViewById(R.id.edtContraseña);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent Homer1 = new Intent(SignUpActivity.this, MainActivity.class);
-                startActivity(Homer1);
+                String nombre = edtNombre.getText().toString().trim();
+                String apodo = edtApodo.getText().toString().trim();
+                String correo = edtCorreo.getText().toString().trim();
+                String contraseña = edtContraseña.getText().toString().trim();
+
+                registrarUsuario(nombre, apodo, correo, contraseña);
             }
         });
 
 
-        registrarButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nombre = nombreEditText.getText().toString();
-                String correo = correoEditText.getText().toString();
-                String contraseña = contraseñaEditText.getText().toString();
+    }
 
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(correo, contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    private void registrarUsuario(final String nombre, final String apodo, String correo, String contraseña) {
+        mAuth.createUserWithEmailAndPassword(correo, contraseña)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String usuarioId = mAuth.getCurrentUser().getUid();
 
+                            DatabaseReference usuarioRef = mDatabase.child("usuarios").child(usuarioId);
+                            usuarioRef.child("nombre").setValue(nombre);
+                            usuarioRef.child("apodo").setValue(apodo);
 
-                        if (task.isSuccessful()){
-
-                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                            if (user != null) {
-                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                        .setDisplayName(nombre)
-                                        .build();
-                                user.updateProfile(profileUpdates);
-                            }
-                            Intent Homer = new Intent(SignUpActivity.this, MainActivity.class);
-                            startActivity(Homer);
-                            // Realiza acciones adicionales después del registro exitoso, si es necesario
                             Toast.makeText(SignUpActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
-
-                        }else {
-                            Toast.makeText(SignUpActivity.this, "Error al registrar el usuario: " + task.getException(), Toast.LENGTH_SHORT).show();
-
+                            finish();
+                        } else {
+                            Toast.makeText(SignUpActivity.this, "Error en el registro: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
-
                     }
                 });
-            }
-        });
-
-
-
-
-
-
     }
 
 
--- app/src/main/java/com/gruposeven/conversoresapp/UserProfileActivity.java
+++ app/src/main/java/com/gruposeven/conversoresapp/UserProfileActivity.java
@@ -1,18 +1,31 @@
 package com.gruposeven.conversoresapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    TextView nombreTextView, correoTextView;
+    private TextView txtNombre;
+    private TextView txtApodo;
+    private TextView txtCorreo;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
 
 
@@ -22,22 +35,41 @@ public class UserProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        nombreTextView = findViewById(R.id.nombreTextView);
-        correoTextView = findViewById(R.id.correoTextView);
 
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApodo = findViewById(R.id.txtApodo);
+        txtCorreo = findViewById(R.id.txtCorreo);
 
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            String usuarioId = currentUser.getUid();
+            DatabaseReference usuarioRef = mDatabase.child("usuarios").child(usuarioId);
 
+            usuarioRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        String nombre = dataSnapshot.child("nombre").getValue(String.class);
+                        String apodo = dataSnapshot.child("apodo").getValue(String.class);
+                        String correo = dataSnapshot.child("correo").getValue(String.class);
 
-        if (user != null) {
-            // Obtén el nombre y correo electrónico del usuario
-            String nombre = user.getDisplayName();
-            String correo = user.getEmail();
+                        txtNombre.setText(nombre);
+                        txtApodo.setText(apodo);
+                        txtCorreo.setText(correo);
+                    }
+                }
 
-            // Muestra los datos en los TextView
-            nombreTextView.setText(nombre);
-            correoTextView.setText(correo);
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Manejar el error de lectura de la base de datos
+                }
+            });
         }
+
     }
+
+
 }
