9e33ff20753049064bb77253dbae3290048d519a
salvadoreandradep <salvadorplay7089@gmail.com>
2023-05-31 10:31:34 -0600

    1

---
+++ app/src/main/java/com/gruposeven/conversoresapp/chats.java
@@ -0,0 +1,14 @@
+package com.gruposeven.conversoresapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chats);
+    }
+}
